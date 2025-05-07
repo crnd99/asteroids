0cbfdf9d64cdcde6b80406d9a384ba737538ad17
crnd99 <crnd99@gmail.com>
2025-05-06 23:04:48 -0500

    very early code for main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import * 
+
+print("Starting Asteroids!")
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    pygame.init()
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT()
+
+        pygame.Surface.fill(screen,"black")
+        pygame.display.flip()
+
